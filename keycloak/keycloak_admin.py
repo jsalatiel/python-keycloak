66aeec668cce4d3273228fa05cacdb02c44ad5f8
jsalatiel <jsalatiel@users.noreply.github.com>
2021-03-23 10:26:45 -0300

    Update keycloak_admin.py

--- keycloak/keycloak_admin.py
+++ keycloak/keycloak_admin.py
@@ -47,7 +47,7 @@ from .urls_patterns import URL_ADMIN_SERVER_INFO, URL_ADMIN_CLIENT_AUTHZ_RESOURC
     URL_ADMIN_REALM_ROLES_MEMBERS, URL_ADMIN_CLIENT_PROTOCOL_MAPPER, URL_ADMIN_CLIENT_SCOPES_MAPPERS, \
     URL_ADMIN_FLOWS_EXECUTIONS_EXEUCUTION, URL_ADMIN_FLOWS_EXECUTIONS_FLOW, URL_ADMIN_FLOWS_COPY, \
     URL_ADMIN_FLOWS_ALIAS, URL_ADMIN_CLIENT_SERVICE_ACCOUNT_USER, URL_ADMIN_AUTHENTICATOR_CONFIG, \
-    URL_ADMIN_CLIENT_ROLES_COMPOSITE_CLIENT_ROLE, URL_ADMIN_CLIENT_ALL_SESSIONS, URL_ADMIN_AUTHENTICATOR_CONFIG_CREATE
+    URL_ADMIN_CLIENT_ROLES_COMPOSITE_CLIENT_ROLE, URL_ADMIN_CLIENT_ALL_SESSIONS, URL_ADMIN_AUTHENTICATOR_CONFIG_CREATE, URL_ADMIN_EVENTS
 
 
 class KeycloakAdmin:
@@ -1422,7 +1422,7 @@ class KeycloakAdmin:
         data_raw = self.raw_put(URL_ADMIN_FLOWS_EXECUTIONS.format(**params_path),
                                 data=json.dumps(payload))
         return raise_error_from_response(data_raw, KeycloakGetError, expected_codes=[202,204])
-
+    
     def create_authentication_flow_execution(self, payload, flow_alias):
         """
         Create an authentication flow execution
@@ -1871,3 +1871,30 @@ class KeycloakAdmin:
         params_path = {"realm-name": self.realm_name, "client-id": client_id}
         data_raw = self.connection.raw_get(URL_ADMIN_CLIENT_ALL_SESSIONS.format(**params_path))
         return raise_error_from_response(data_raw, KeycloakGetError)
+    
+    
+    def update_events_config(self, payload ):
+        """
+        Update event config
+
+        :param payload: AuthenticationExecutionInfoRepresentation
+        :return: Keycloak server response
+        """
+
+        params_path = {"realm-name": self.realm_name}
+        data_raw = self.raw_put(URL_ADMIN_EVENTS.format(**params_path),
+                                data=json.dumps(payload))
+        return raise_error_from_response(data_raw, KeycloakGetError, expected_codes=[202,204])
+    
+    def get_events_config(self):
+        """
+        get event config
+
+        :return: Keycloak server response
+        """
+
+        params_path = {"realm-name": self.realm_name}
+        data_raw = self.raw_get(URL_ADMIN_EVENTS.format(**params_path))
+        return raise_error_from_response(data_raw, KeycloakGetError, expected_codes=[200])
+
+    
